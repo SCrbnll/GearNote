7baadd5098d55e85b95882fd57cfd9a0819fb4ac
SCrbnll <samuelcarbonell1203@gmail.com>
2025-06-22 02:42:35 +0200

    [MOD] NativeWind Added

---
+++ app/index.tsx
@@ -0,0 +1,17 @@
+import { Text, View } from "react-native";
+import '../global.css';
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text className="bg-green-700 text-white">TEST</Text>
+    </View>
+  );
+}
